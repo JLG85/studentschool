f00d968d7378a8cc621e7797b5f1acdc84828be8
JLG85 <35577860+JLG85@users.noreply.github.com>
2018-11-13 14:30:22 -0600

    Add files via upload

---
+++ controllers/schoolController.js
@@ -0,0 +1,102 @@
+var School = require('../models/school');
+var School = require('../models/student');
+
+exports.school_get = function(req, res) {
+    let filters = req.query
+
+	School.find(filters)
+	.then(schools => {
+		res.json({
+			confirmation: 'success',
+			data: schools
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.school_update = function(req, res) {
+    const query = req.query // require: id, key=value
+	const schoolId = query.id
+	delete query['id']
+
+	School.findByIdAndUpdate(schoolId, query, {new:true})
+	.then(school => {
+		res.json({
+			confirmation: 'success',
+			data: school
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.school_remove = function(req, res) {
+    const query = req.query
+
+	Student.find({'school':query.id})
+    .then(students => {
+        if (students.length>0) {
+            res.json({
+                confirmation: 'sucess',
+                message:'School has students. Can not be deleted',
+                data: students
+            })
+        } else {
+            school.findByIdAndRemove(query.id)
+            .then(data => {
+                res.json({
+                    confirmation: 'success',
+                    data: 'School'+query.id+' sucessfully removed.'
+                })
+            })
+            .catch(err => {
+                res.json({
+                    confirmation: 'fail',
+                    message: err.message
+                })
+            })
+        }
+    })
+}
+
+exports.school_id = function(req, res) {
+    const id = req.params.id
+
+	School.findById(id)
+	.then(student => {
+		res.json({
+			confirmation: 'success',
+			data: School	})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'School ' + id + ' not found.'
+		})
+	})
+}
+
+exports.school_post = function(req, res) {
+    School.create(req.body)
+	.then(school => {
+		res.json({
+			confirmation: 'success',
+			data: School
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
